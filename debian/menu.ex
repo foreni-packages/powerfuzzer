720f1d985252580e0ec3b28c39ecdbd32ec4fc55
Devon Kearns <dookie@kali.org>
2013-01-02 11:13:39 -0700

    Commit before patching

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(powerfuzzer):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="powerfuzzer" command="/usr/bin/powerfuzzer"
--- debian/powerfuzzer.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the powerfuzzer package
-#
-0 4	* * *	root	[ -x /usr/bin/powerfuzzer_maintenance ] && /usr/bin/powerfuzzer_maintenance
